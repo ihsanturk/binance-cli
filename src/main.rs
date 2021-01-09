1514329e3391ef3f230630540ae95b1381fefd2a
ihsan <ihsanl@pm.me>
2021-01-09 22:10:51 +0300

    add: clap as argument parser, shorten program name

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,79 @@
 extern crate binance;
+#[macro_use]
+extern crate clap;
 
 use binance::api::*;
 use binance::account::*;
+use clap::{Arg, App, AppSettings};
 
 fn main() {
 
-	let api_key_env = "BINANCE_API_KEY";
-	let api_key: Option<String> = match std::env::var(api_key_env) {
-		Ok(value) => Some(value),
-		Err(e) => {
-			eprintln!("{}: {:?}", e, api_key_env);
-			None
-		},
-	};
+	let matches = App::new(crate_name!())
+		.setting(AppSettings::ColoredHelp)
+		.version(crate_version!())
+		.author(crate_authors!())
+		.about(crate_description!())
 
-	let sec_key_env = "BINANCE_SEC_KEY";
-	let secret_key: Option<String> = match std::env::var(sec_key_env) {
-		Ok(value) => Some(value),
-		Err(e) => {
-			eprintln!("{}: {:?}", e, sec_key_env);
-			None
-		},
-	};
+		.subcommand(App::new("balance")
+			.setting(AppSettings::ColoredHelp)
+			.arg(Arg::new("asset")
+				.short('a')
+				.long("asset")
+				.required(true)
+				.about("Asset, ex: btc")
+				.takes_value(true)))
 
-	let account: Account = Binance::new(api_key, secret_key);
+		.subcommand(App::new("trades")
+			.setting(AppSettings::ColoredHelp)
+			.arg(Arg::new("symbol")
+				.short('s')
+				.long("symbol")
+				.required(true)
+				.about("Symbol, ex: btcusdt")
+				.takes_value(true))
+			.arg(Arg::new("output")
+				.short('o')
+				.long("output")
+				.required(false)
+				.about("Output format, ex: ledger")
+				.takes_value(true)))
 
-	match account.trade_history("ETHBTC") {
-		Ok(answer) => println!("{:?}", answer),
-		Err(e) => println!("Error: {}", e),
-	};
+		.get_matches();
+
+	let account: Account = Binance::new(
+		get_env_var("BINANCE_API_KEY"),
+		get_env_var("BINANCE_SEC_KEY")
+	);
+
+	// balance
+	if let Some(ref matches) = matches.subcommand_matches("balance") {
+		let asset = matches.value_of("asset");
+		match account.get_balance(asset.unwrap().to_uppercase()) {
+			Ok(response) => println!("{:?}", response), // format_output(response, "ledger")
+			Err(e) => println!("Error: {}", e),
+		};
+	}
+
+	// trades
+	else if let Some(ref matches) = matches.subcommand_matches("trades") {
+		let symbol = matches.value_of("symbol");
+		match account.trade_history(symbol.unwrap().to_uppercase()) {
+			Ok(response) => println!("{:?}", response),
+			Err(e) => println!("Error: {}", e),
+		};
+	}
 
 }
+
+fn get_env_var(key: &str) -> Option<String> {
+	match std::env::var(key) {
+		Ok(value) => Some(value),
+		Err(e) => {
+			eprintln!("{}: {:?}", e, key);
+			std::process::exit(2);
+		},
+	}
+}
+
+// fn output_as_ledger_format(trades) {
+// }
